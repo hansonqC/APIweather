ced13eb5bf2c0403e968b56b8f6bc0b5be14a3fa
hansonq <lcacbcac@gmail.com>
2017-09-24 12:22:19 +0200

    Initial commit

--- src/main/java/pl/hansonq/models/dao/impl/WeatherDaoImpl.java
+++ src/main/java/pl/hansonq/models/dao/impl/WeatherDaoImpl.java
@@ -1,7 +1,47 @@
 package pl.hansonq.models.dao.impl;
 
+import pl.hansonq.models.MysqlConnector;
+import pl.hansonq.models.WeatherModel;
+import pl.hansonq.models.dao.WeatherDao;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * Created by lukasz on 2017-09-24.
  */
-public class WeatherDaoImpl {
+public class WeatherDaoImpl implements WeatherDao{
+
+
+    private MysqlConnector connector = MysqlConnector.getInstance();
+    @Override
+    public void addWeather(WeatherModel model) {
+        try {
+            PreparedStatement preparedStatement = connector.getConnection().prepareStatement(
+                    "INSERT INTO weather VALUES(?,?,?,?)"
+            );
+
+            preparedStatement.setInt(1, 0);
+            preparedStatement.setString(2, model.getCityname());
+            preparedStatement.setFloat(3, model.getTemp());
+            preparedStatement.setDate(4, model.getDate());
+
+            preparedStatement.execute();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public List<WeatherModel> getAllWeatherData(String cityname) {
+        return null;
+    }
+
+    @Override
+    public List<String> getCities() {
+        return null;
+    }
 }
